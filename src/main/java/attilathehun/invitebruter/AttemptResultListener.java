4353f602ed238f475d979b7d8a38243f80cccf9e
BlackDragonTechnology <blackdragontechnology@gmail.com>
2021-04-29 10:24:57 +0200

    Initial v2.0 commit

--- src/main/java/attilathehun/invitebruter/AttemptResultListener.java
+++ src/main/java/attilathehun/invitebruter/AttemptResultListener.java
@@ -1,15 +1,6 @@
-<<<<<<< HEAD
 package attilathehun.invitebruter;
 
 public interface AttemptResultListener {
     void attemptResulted(String messageToLog, boolean toDump);
     void logMessage(String message);
 }
-=======
-package attilathehun.invitebruter;
-
-public interface AttemptResultListener {
-    void attemptResulted(String messageToLog, boolean toDump);
-    void logMessage(String message);
-}
->>>>>>> 02c222fe7bfdc69c01f2b58d027fe37fe080bbd8
